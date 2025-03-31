3222d61eeded1a94c0a0d7b37628766f4e3aa953
Arek <arek93r@gmail.com>
2025-03-31 17:14:36 +0200

    localStorage dla pola wyszukiwania

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,18 @@ export function SearchBar(onSearch) {
     const input = document.createElement('input')
     input.type = 'text'
     input.placeholder = 'Szukaj użytkownika...'
-    input.addEventListener('input', () => onSearch(input.value))
+
+    //wczytaj poprzednią wartość
+    input.value = localStorage.getItem("searchQuery") || ""
+    
+    input.addEventListener('input', () => {
+        const query = input.value
+        localStorage.setItem("searchQuery", query) //dodanie do localstorage
+        onSearch(query)
+    })
+
+    //wywołaj od razu przy starcie
+    setTimeout(() => onSearch(input.value), 0)
+    
     return input
 }
